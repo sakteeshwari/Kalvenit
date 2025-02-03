f247a66388babf16facbdd191f73335ff74597fe
sakteeshwari <sakthijessy26@gmail.com>
2025-02-03 12:41:06 +0530

    added

--- app/pages/index.js
+++ app/pages/index.js
@@ -5,7 +5,7 @@ import Video from "../components/video/Video";
 import Partner from "../components/Partner/Partner";
 import Services from "../components/Servicescomp/Servicescomp";
 import CoEs from "../components/Coescomp/Coescomp";
-import Solutions from "../components/Solutions/Solutions";
+import Solutions from "../components/Solutionscomp/Solutionscomp";
 import Befuture from "../components/Befuture/Befuture";
 import Careers from "../components/Careers/Careers";
 import Address from "../components/Address/Address";
