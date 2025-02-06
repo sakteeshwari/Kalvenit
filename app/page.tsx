30d72728ce32445d24ec07bca30134159c9c2e55
sakteeshwari <sakthijessy26@gmail.com>
2025-02-06 17:47:46 +0530

    added

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Navbar from "./components/Navbar/Navbar.jsx"
 import Footer from "./components/Footer/Footer.jsx"
 import Homepage from "./pages/index.js"
+
 // import Servicepage from "./pages/services.js"
 // import Link from 'next/link';
 // import { useRouter } from 'next/navigation'
--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,5 +1,5 @@
-""
-import "../../app/globals.css"
+"use client"
+import '../globals.css'; // Adjust the path if needed
 import Navbar from "../components/Navbar/Navbar.jsx";
 import Footer from "../components/Footer/Footer.jsx";
 
