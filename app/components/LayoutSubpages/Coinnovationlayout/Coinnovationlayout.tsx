76e097ec91fc1bb34d5914d4769702812e1c1b08
sakteeshwari <sakthijessy26@gmail.com>
2025-02-13 17:02:45 +0530

    added

--- app/components/LayoutSubpages/Coinnovationlayout/Coinnovationlayout.tsx
+++ app/components/LayoutSubpages/Coinnovationlayout/Coinnovationlayout.tsx
@@ -1,1 +1,22 @@
-c
+"use client"
+import React from "react";
+import Servicespageimg from "../../Servicespageimg/Servicespageimg";
+import Coinnovationlab from "../../../components/Subpages/Coinnovationlab/Coinnovationlab"
+import Servicesperspective from "../../Servicesperspective/Servicesperspective";
+import Biznextpagespotlight from "../../Biznextpagespotlight/Biznextpagespotlight";
+import Kalvendifferent from "../../Kalvendifferent/Kalvendifferent";
+
+const Mordernizationlayout = ({ children, imgSrc }: { children: React.ReactNode; imgSrc: string }) => {
+  return (
+    <div>
+      <Servicespageimg imgSrc={imgSrc}  text=" Embrace the Future of Intelligent Cloud & Infrastructure with KALVEN"  />
+      {children}
+      <Coinnovationlab></Coinnovationlab>
+      <Biznextpagespotlight />
+      <Kalvendifferent />
+      <Servicesperspective />
+    </div>
+  );
+};
+
+export default Mordernizationlayout;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
+import { FlatCompat } from "@eslint/eslintrc";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +9,14 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default {
+  extends: [
+    "next/core-web-vitals",
+    "next/typescript",
+  ],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+    "@next/next/no-img-element": "off",
+    "react-hooks/exhaustive-deps": "off",
+  },
+};
