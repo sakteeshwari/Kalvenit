74ad5ea8210faffcfb718bb5a286701ebf20e741
sakteeshwari <sakthijessy26@gmail.com>
2025-02-13 16:14:23 +0530

    added

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
     extends: "next/core-web-vitals",
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
+       "@next/next/no-img-element": "off",
+       
     },
   };
   
