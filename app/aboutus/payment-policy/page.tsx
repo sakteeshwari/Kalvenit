db69ce2e4411086a853e65c626e052f761bd2554
sakteeshwari <sakthijessy26@gmail.com>
2025-02-20 17:42:08 +0530

    added

--- app/aboutus/payment-policy/page.tsx
+++ app/aboutus/payment-policy/page.tsx
@@ -20,9 +20,9 @@ const page = () => {
           <h2 className="text-xl font-semibold mb-2">Introduction</h2>
           <p className="leading-relaxed">
             These terms and conditions apply to the User who uses the Online Services provided for any payment made
-            to <strong>Yash Technologies Private Limited (“YASH”)</strong>. Kindly read these terms and conditions carefully.
-            By authorizing a payment to YASH through the online payment service, it would be treated as a deemed 
-            acceptance of these terms and conditions. YASH reserves all rights to amend these terms and conditions 
+            to <strong>KALVEN Software Solutions Private Limited (“KALVEN”)</strong>. Kindly read these terms and conditions carefully.
+            By authorizing a payment to KALVEN through the online payment service, it would be treated as a deemed 
+            acceptance of these terms and conditions. KALVEN reserves all rights to amend these terms and conditions 
             at any time without prior notice. It is the responsibility of the User to have read the terms and conditions 
             before using the Service.
           </p>
@@ -40,23 +40,23 @@ const page = () => {
             </li>
             <li>
               The credit/debit card information supplied at the time of using the service is processed by the 
-              payment gateway of the service provider and is not supplied to YASH. It is the sole responsibility 
+              payment gateway of the service provider and is not supplied to KALVEN. It is the sole responsibility 
               of the User to ensure that the information entered is correct.
             </li>
             <li>
               The Customer agrees, understands, and confirms that their personal data, including debit/credit card 
-              details transmitted over the Internet, may be susceptible to misuse, hacking, theft, or fraud. YASH or 
+              details transmitted over the Internet, may be susceptible to misuse, hacking, theft, or fraud. KALVEN or 
               the Payment Service Provider(s) have no control over such matters.
             </li>
             <li>
-            The service is provided using a payment gateway service provider through a secure website. However, neither the payment gateway service provider nor the YASH gives any assurance, that the information so provided online by a user is secured or may be read or intercepted by a third party. YASH does not accept or assume any liability in the event of such unauthorized interception, hacking or other unauthorized access to information provided by a user of the service.
+            The service is provided using a payment gateway service provider through a secure website. However, neither the payment gateway service provider nor the KALVEN gives any assurance, that the information so provided online by a user is secured or may be read or intercepted by a third party. KALVEN does not accept or assume any liability in the event of such unauthorized interception, hacking or other unauthorized access to information provided by a user of the service.
             </li>
             <li>
-            YASH and/or the Payment Service Providers shall not be liable for any inaccuracy, error or delay in, or omission of (a) any data, information or message, or (b) the transmission or delivery of any such data, information or message; or (c) any loss or damage arising from or occasioned by any such inaccuracy, error, delay or omission, non-performance or interruption in any such data, information or message. Under no circumstances shall the YASH and/or the Payment Service Providers, its employees, directors, and its third party agents involved in processing, delivering or managing the Services, be liable for any direct, indirect, incidental, special or consequential damages, or any damages whatsoever, including punitive or exemplary arising out of or in any way connected with the provision of or any inadequacy or deficiency in the provision of the Services or resulting from unauthorized access or alteration of transmissions of data or arising from suspension or termination of the Service.
+            KALVEN and/or the Payment Service Providers shall not be liable for any inaccuracy, error or delay in, or omission of (a) any data, information or message, or (b) the transmission or delivery of any such data, information or message; or (c) any loss or damage arising from or occasioned by any such inaccuracy, error, delay or omission, non-performance or interruption in any such data, information or message. Under no circumstances shall the KALVEN and/or the Payment Service Providers, its employees, directors, and its third party agents involved in processing, delivering or managing the Services, be liable for any direct, indirect, incidental, special or consequential damages, or any damages whatsoever, including punitive or exemplary arising out of or in any way connected with the provision of or any inadequacy or deficiency in the provision of the Services or resulting from unauthorized access or alteration of transmissions of data or arising from suspension or termination of the Service.
 
             </li>
             <li>
-            The Customer agrees that YASH or any of its employees will not be held liable By the Customer for any loss or damages arising from your use of, or reliance upon the information contained on the Website, or any failure to comply with these Terms and Conditions where such failure is due to circumstance beyond YASH’s reasonable control.
+            The Customer agrees that KALVEN or any of its employees will not be held liable By the Customer for any loss or damages arising from your use of, or reliance upon the information contained on the Website, or any failure to comply with these Terms and Conditions where such failure is due to circumstance beyond KALVEN’s reasonable control.
             </li>
           </ul>
         </section>
@@ -65,7 +65,7 @@ const page = () => {
         <h2 className="text-xl font-semibold mb-2">Debit/Credit Card, Bank Account Details:</h2>
         <ul className="list-disc pl-6 space-y-3">
           <li>The Customer agrees that the debit/credit card details provided by them for use of the aforesaid Service(s) must be correct and accurate and that the Customer shall not use a Debit/ credit card, that is not lawfully owned by them or the use of which is not authorized by the lawful owner thereof. The Customer further agrees and undertakes to Provide correct and valid debit/credit card details.</li>
-          <li>The Customer may pay their outstanding dues to YASH by using a debit/credit card or through online banking account. The Customer warrants, agrees and confirms that when they initiates a payment transaction and/or issues an online payment instruction and provides their card / bank details:</li>
+          <li>The Customer may pay their outstanding dues to KALVEN by using a debit/credit card or through online banking account. The Customer warrants, agrees and confirms that when they initiates a payment transaction and/or issues an online payment instruction and provides their card / bank details:</li>
               <ul className="list-decimal pl-6 space-y-3">
                 <li>The Customer is fully and lawfully entitled to use such credit / debit card, bank account for such transactions;</li>
                 <li>The Customer is responsible to ensure that the card/ bank account details provided by them are accurate;</li>
@@ -79,28 +79,28 @@ const page = () => {
       <h1 className="text-2xl font-bold">TERMS AND CONDITIONS FOR ONLINE PAYMENTS</h1>
       
       <h2 className="mt-4 text-xl font-semibold">Introduction:</h2>
-      <p className="mt-2">These terms and conditions apply to the User who uses the Online Services provided for any payment made to Yash Technologies Private Limited "YASH". Kindly read these terms and conditions carefully. By authorizing a payment to YASH through the online payment service ("the service"), it would be treated as a deemed acceptance to these terms and conditions. YASH reserves all the rights to amend these terms and conditions at any time without giving prior notice. It is the responsibility of the User to have read the terms and conditions before using the Service.</p>
+      <p className="mt-2">These terms and conditions apply to the User who uses the Online Services provided for any payment made to KALVEN Software Solutions Private Limited "KALVEN". Kindly read these terms and conditions carefully. By authorizing a payment to KALVEN through the online payment service ("the service"), it would be treated as a deemed acceptance to these terms and conditions. KALVEN reserves all the rights to amend these terms and conditions at any time without giving prior notice. It is the responsibility of the User to have read the terms and conditions before using the Service.</p>
       
       <h2 className="mt-4 text-xl font-semibold">Key terms:</h2>
       <ul className="list-disc list-inside mt-2">
         <li>Payment(s) through this Service may only be made with a Credit Card, Debit card, or Net Banking.</li>
         <li>Before using this Service, it is recommended that the user shall make necessary enquiry about the charges or fees payable against the Credit/Debit card used from Credit Card or the Debit Card service provider i.e. the respective Bank.</li>
-        <li>The credit/debit card information supplied at the time of using the service is processed by the payment gateway of the service provider and is not supplied to YASH.</li>
+        <li>The credit/debit card information supplied at the time of using the service is processed by the payment gateway of the service provider and is not supplied to KALVEN.</li>
         <li>The Customer agrees, understands and confirms that their personal data including details relating to debit card/credit card/net banking transmitted over the Internet may be susceptible to misuse.</li>
         <li>The service is provided using a payment gateway service provider through a secure website.</li>
       </ul>
       
       <h2 className="mt-4 text-xl font-semibold">No warranty:</h2>
-      <p className="mt-2">No warranty, representation or guarantee, express or implied, is given by the YASH in respect of the operation of the service.</p>
+      <p className="mt-2">No warranty, representation or guarantee, express or implied, is given by the KALVEN in respect of the operation of the service.</p>
       
       <h2 className="mt-4 text-xl font-semibold">Disclaimer and Limitation of liability:</h2>
-      <p className="mt-2">YASH does not accept liability for any damage, loss, cost (including legal costs), expenses, indirect losses or consequential damage of any kind which may be suffered or incurred by the User from the use of this service.</p>
+      <p className="mt-2">KALVEN does not accept liability for any damage, loss, cost (including legal costs), expenses, indirect losses or consequential damage of any kind which may be suffered or incurred by the User from the use of this service.</p>
       
       <h2 className="mt-4 text-xl font-semibold">Governing law:</h2>
       <p className="mt-2">These terms and conditions are governed by the laws of India and the competent courts at Indore shall have exclusive jurisdiction.</p>
       
       <h2 className="mt-4 text-xl font-semibold">Dispute Resolution:</h2>
-      <p className="mt-2">Any dispute arising in relation to the terms and conditions will be submitted to a sole arbitrator appointed by YASH under the provisions of the Arbitration and Conciliation Act, 1996.</p>
+      <p className="mt-2">Any dispute arising in relation to the terms and conditions will be submitted to a sole arbitrator appointed by KALVEN under the provisions of the Arbitration and Conciliation Act, 1996.</p>
       
       <h2 className="mt-4 text-xl font-semibold">Refund/Cancellation Policy:</h2>
       <p className="mt-2">Payment once made will not be refunded under any circumstances.</p>
@@ -116,7 +116,7 @@ const page = () => {
             The information provided by the user on this website shall be kept confidential and shall not be shared with anyone.
           </li>
           <li>
-            YASH may share your personal information with our bankers for clearing necessary fees payments.
+            KALVEN may share your personal information with our bankers for clearing necessary fees payments.
           </li>
           <li>
             If payment is made by means of a card that you do not personally own, the permission of the card owner must always be obtained.
@@ -125,7 +125,7 @@ const page = () => {
             Information related to payments using debit or credit card is not accessed or stored by the Institute.
           </li>
           <li>
-            Limitation of Liability: In no event, YASH will be liable for any damage, direct or indirect, losses or expenses.
+            Limitation of Liability: In no event, KALVEN will be liable for any damage, direct or indirect, losses or expenses.
           </li>
         </ul>
       </section>
@@ -134,18 +134,18 @@ const page = () => {
         <h2 className="font-bold text-lg">Security:</h2>
         <p className="mt-2">
           All payment details entered through this payment gateway are encrypted with 256-bit encryption to ensure secure communications. 
-          Yash Technologies Private Limited shall not be liable for any failure by the customer or third party making payment.
+          KALVEN Software Solutions Private Limited shall not be liable for any failure by the customer or third party making payment.
         </p>
       </section>
 
       <section className="mb-6">
         <h2 className="font-bold text-lg">Variations to the Terms and Conditions:</h2>
         <p className="mt-2">
-          The YASH reserves the right to vary these Terms and Conditions from time to time and the current version will be published on this website.
+          The KALVEN reserves the right to vary these Terms and Conditions from time to time and the current version will be published on this website.
         </p>
         <p className="mt-2">
           We reserve the right to decline the acceptance of an online payment if your account is in default. 
-          Yash Technologies Private Limited may also make modifications without prior notice.
+          KALVEN Software Solutions Private Limited may also make modifications without prior notice.
         </p>
       </section>
     </div>
