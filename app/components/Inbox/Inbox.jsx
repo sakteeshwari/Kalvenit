3b6af48cd94993dc20efe898631e8152ddef9d13
sakteeshwari <sakthijessy26@gmail.com>
2025-02-21 18:07:51 +0530

    added

--- app/components/Inbox/Inbox.jsx
+++ app/components/Inbox/Inbox.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import "./Inbox.css"
-import { ArrowLeft, ArrowRight } from "lucide-react";
+import React from 'react';
+import Link from 'next/link';
+import "./Inbox.css";
+import { ArrowRight } from "lucide-react";
 
 const Inbox = () => {
     return (
@@ -10,25 +11,24 @@ const Inbox = () => {
             </div>
             <div className='flex flex-col'>
                 <h1 className='text-white'>Stay connected to get the best of Kalven Software Solutions in your inbox</h1>
-                <div className=" py-3 text-lg slide-up">
-                    <button className="relative flex items-center space-x-2 text-yellow-300 font-medium group hover:rounded-3xl hover:px-3 hover:py-1 transition-all duration-300 overflow-hidden">
-                        {/* Border animation */}
-                        <div className="absolute inset-0 w-0 overflow-hidden group-hover:w-full h-full bg-transparent border border-yellow-300 rounded-3xl transition-all duration-500 ease-in-out opacity-0 group-hover:opacity-100"></div>
+                <div className="py-3 text-lg slide-up">
+                    <Link href="/contact" passHref>
+                        <button className="relative flex items-center space-x-2 text-yellow-300 font-medium group hover:rounded-3xl hover:px-3 hover:py-1 transition-all duration-300 overflow-hidden">
+                            <div className="absolute inset-0 w-0 overflow-hidden group-hover:w-full h-full bg-transparent border border-yellow-300 rounded-3xl transition-all duration-500 ease-in-out opacity-0 group-hover:opacity-100"></div>
 
-
-                        {/* Content */}
-                        <div className="flex justify-center items-center w-8 h-8 border border-yellow-300 rounded-full group-hover:hidden z-10">
-                            <ArrowRight className="w-4 h-4" />
-                        </div>
-                        <div className="hidden group-hover:block z-10">
-                            <span>&rarr;</span>
-                        </div>
-                        <span className="z-10">Know more</span>
-                    </button>
+                            <div className="flex justify-center items-center w-8 h-8 border border-yellow-300 rounded-full group-hover:hidden z-10">
+                                <ArrowRight className="w-4 h-4" />
+                            </div>
+                            <div className="hidden group-hover:block z-10">
+                                <span>&rarr;</span>
+                            </div>
+                            <span className="z-10">Catch Up</span>
+                        </button>
+                    </Link>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Inbox
+export default Inbox;
