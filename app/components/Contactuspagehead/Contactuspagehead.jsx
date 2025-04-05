8a6603fea214536e5ba3aaa4ad6e3b47009e8809
sakteeshwari <sakthijessy26@gmail.com>
2025-04-05 10:05:06 +0530

    added

--- app/components/Contactuspagehead/Contactuspagehead.jsx
+++ app/components/Contactuspagehead/Contactuspagehead.jsx
@@ -2,13 +2,13 @@
 
 export default function GlobalHeadquarters() {
   return (
-    <div className="bg-white my-10  shadow-md border border-blue-300 rounded-lg p-8 max-w-4xl mx-4 lg:mx-auto flex flex-col md:flex-row md:items-center md:space-x-8">
+    <div className="bg-white my-10 shadow-md border border-blue-300 rounded-lg p-8 max-w-4xl mx-4 lg:mx-auto flex flex-col md:flex-row md:items-center md:space-x-8">
       {/* Left Section */}
       <div className="flex-1 text-center md:text-left">
         <h2 className="text-2xl font-bold text-gray-800">
           Global Headquarters<span className="text-red-500">.</span>
         </h2>
-        <p className="text-gray-500 mt-2">Corporate Office</p>
+        <p className="text-gray-500 my-2">Corporate Office</p>
       </div>
 
       {/* Right Section */}
@@ -23,15 +23,22 @@ export default function GlobalHeadquarters() {
           </div>
           <h3 className="text-lg font-semibold">United States</h3>
         </div>
-        <p>7220 McCallum Blvd,</p>
-        <p>Unit 1109</p>
-        <p>Dallas 75252</p>
-
-       
+        <p><b>Address:</b> &nbsp; 5900 Balcones Drive STE 100</p>
+        <p>Austin, TX 78731</p>
 
         <div className="mt-4">
-          <h4 className="font-semibold">Sales & Marketing phone:</h4>
-          <p className="text-purple-600">+1 309-755-0433</p>
+          <h4>
+            <b>Contact No:  &nbsp; </b>
+            <a href="tel:+13097550433" className="text-purple-600 hover:underline">
+              +1 309-755-0433
+            </a>
+          </h4>
+          <p className="mt-4">
+            <b>Email ID: &nbsp; </b>
+            <a href="mailto:immigration@kalvenit.com" className="text-purple-600 hover:underline">
+               immigration@kalvenit.com
+            </a>
+          </p>
         </div>
       </div>
     </div>
